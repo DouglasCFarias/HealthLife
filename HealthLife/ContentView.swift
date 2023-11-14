4f688b54a37c959413121f18e889d57527417d4b
DylanSkywalker <114362937+DylanSkywalker@users.noreply.github.com>
2023-11-14 19:31:17 -0300

    Setup da arquitetura

--- HealthLife/ContentView.swift
+++ HealthLife/ContentView.swift
@@ -84,3 +84,5 @@ private let itemFormatter: DateFormatter = {
 #Preview {
     ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
 }
+
+//se of unknown directive '#Preview'
