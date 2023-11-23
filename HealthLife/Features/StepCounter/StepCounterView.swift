d0075bea05e672746b61f2224647f2b5bbcb5c62
Douglas <108027630+DouglasCFarias@users.noreply.github.com>
2023-11-22 21:32:29 -0300

    Timer added

--- HealthLife/Features/StepCounter/StepCounterView.swift
+++ HealthLife/Features/StepCounter/StepCounterView.swift
@@ -1,16 +1,42 @@
 import SwiftUI
 import MapKit
+import CoreLocation
+
 struct StepCounterView: View {
+    @State var timernow = Date.now
+    @State var region = MKCoordinateRegion(center: .init(latitude: -3.720747, longitude: -38.512636), span: .init(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    
+    let locationManager = CLLocationManager()
+
     var body: some View {
-        NavigationStack {
-            Map {
-                
-            }
-            .frame(width: 394, height: 419)
-            .padding(.bottom, 250)
+        Map (
+            coordinateRegion: $region,
+            showsUserLocation: true,
+            userTrackingMode: .constant(.follow)
+        )
+        .onAppear {
+            locationManager.requestWhenInUseAuthorization()
         }
+        .frame(width: 394, height: 419)
+        .padding(.bottom, 250)
+        // Spacer()
+        VStack{
+            Text("Tempo")
+                .bold()
+            
+        }
+        .padding(.top, -250)
+        .font(.system(size: 15))
+        
+        
+        
         .navigationTitle("Step Counter")
         .navigationBarTitleDisplayMode(.inline)
+        //        .toolbar {
+        //            ToolbarItem(placement: .navigationBarLeading) {
+        //                HomeView()
+        //            }
+        //     }
     }
 }
 
